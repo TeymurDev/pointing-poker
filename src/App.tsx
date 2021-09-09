41a25c26473a83ca4cd7fa8bba63bc718965af25
snegurova <snegurova@gmail.com>
2021-09-09 10:10:56 +0300

    refactor: create subscribeToConnection and move to api

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { FC, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
-import socket from './api';
+import subscribeToConnection from './api';
 
 const App: FC = () => {
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log(`Socket id: ${socket.id} connected`);
-    });
+    subscribeToConnection();
   }, []);
   return (
     <div className='container'>
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,4 +3,10 @@ import io from 'socket.io-client';
 const port = 5000;
 const socket = io(`http://localhost:${port}`);
 
-export default socket;
+const subscribeToConnection = () => {
+  socket.on('connect', () => {
+    console.log(`Socket id: ${socket.id} connected`);
+  });
+};
+
+export default subscribeToConnection;
