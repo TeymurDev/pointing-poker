857f4f6cf1d4facd489159499e801093bb8732ee
Olga Bashlaminova <olga.bashlaminova.ob@gmail.com>
2021-09-13 12:21:04 +0100

    feat: finish creating Link To Lobby Component

---
+++ src/components/LinkToLobby/link-to-lobby.tsx
@@ -0,0 +1,26 @@
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Space, Input, Button, message } from 'antd';
+import styles from './link-to-lobby.module.css';
+
+type LinkToLobbyInfo = {
+  link: string;
+};
+
+const LinkToLobby = ({ link }: LinkToLobbyInfo) => {
+  const success = () => {
+    message.success('Copied!');
+  };
+
+  return (
+    <div>
+      <Space className={styles.container}>
+        <Input value={link} readOnly />
+        <CopyToClipboard text={link} onCopy={success}>
+          <Button type='primary'>Copy</Button>
+        </CopyToClipboard>
+      </Space>
+    </div>
+  );
+};
+
+export default LinkToLobby;
