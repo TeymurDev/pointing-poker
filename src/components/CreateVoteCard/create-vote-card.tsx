f0337eded4e3c62aa1759e71547b964a7a00154e
Olga Bashlaminova <olga.bashlaminova.ob@gmail.com>
2021-09-28 15:41:16 +0100

    feat: create vote cards

---
+++ src/components/CreateVoteCard/create-vote-card.tsx
@@ -0,0 +1,20 @@
+import { Space, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import styles from './create-vote-card.module.css';
+
+const CreateVoteCard = () => {
+  return (
+    <div>
+      <Space size={15} align='center' className={styles.container}>
+        <Button
+          shape='circle'
+          type='text'
+          icon={<PlusOutlined />}
+          className={styles.addButton}
+        />
+      </Space>
+    </div>
+  );
+};
+
+export default CreateVoteCard;
