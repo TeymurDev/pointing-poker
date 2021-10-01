12deee4730d3fe173c40c9e224880c05f4ebb2b4
Olga Bashlaminova <olga.bashlaminova.ob@gmail.com>
2021-10-01 18:04:46 +0100

    feat: finish creating Member Card Mini

---
+++ src/components/MemberCardMini/member-card-mini.tsx
@@ -0,0 +1,46 @@
+import { Space, Button, Avatar, Typography } from 'antd';
+import { UserOutlined, StopOutlined } from '@ant-design/icons';
+import styles from './member-card-mini.module.css';
+
+import { MemberCardInfo } from '../MemberCard/member-card';
+
+const { Text } = Typography;
+
+const MemberCardMini = ({
+  id,
+  firstName,
+  lastName,
+  position,
+  image,
+}: MemberCardInfo) => {
+  return (
+    <div>
+      <Space align='center' className={styles.container}>
+        <Space size={15} align='center'>
+          <Avatar
+            size='default'
+            src={image}
+            icon={<UserOutlined />}
+            className={styles.avatar}
+          />
+          <div className={styles.textContainer}>
+            {/* <Text className={styles.textPertain}>It&apos;s you</Text> */}
+            <Text className={styles.textName}>
+              {firstName} {lastName} {id}
+            </Text>
+            <Text className={styles.textPost}>{position}</Text>
+          </div>
+        </Space>
+        <Button
+          shape='circle'
+          size='large'
+          type='text'
+          icon={<StopOutlined />}
+          className={styles.deleteButton}
+        />
+      </Space>
+    </div>
+  );
+};
+
+export default MemberCardMini;
